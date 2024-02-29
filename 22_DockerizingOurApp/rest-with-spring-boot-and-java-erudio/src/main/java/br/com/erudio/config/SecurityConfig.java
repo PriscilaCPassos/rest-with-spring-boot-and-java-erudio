33ad4c2f7d3bc3c78f2dc32a21e3be9c1fdf66be
PriscilaCPassos <116843833+PriscilaCPassos@users.noreply.github.com>
2024-02-29 15:59:58 -0300

    Novas atualizações

--- 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
+++ 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -3,13 +3,9 @@ package br.com.erudio.config;
 import java.util.HashMap;
 import java.util.Map;
 
-import br.com.erudio.security.Jwt.JwtTokenFilter;
-import br.com.erudio.security.Jwt.JwtTokenProvider;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,9 +15,11 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.erudio.security.Jwt.JwtTokenFilter;
+import br.com.erudio.security.Jwt.JwtTokenProvider;
 
 @EnableWebSecurity
 @Configuration
@@ -34,25 +32,25 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         Map<String, PasswordEncoder> encoders = new HashMap<>();
 
-        Pbkdf2PasswordEncoder pbkdf2Encoder =
-                new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
         encoders.put("pbkdf2", pbkdf2Encoder);
-        DelegatingPasswordEncoder passwordEncoder =
-                new DelegatingPasswordEncoder("pbkdf2", encoders);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
         return passwordEncoder;
     }
 
     @Bean
     AuthenticationManager authenticationManagerBean(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+
         //@formatter:off
         return http
                 .httpBasic(basic -> basic.disable())
@@ -76,5 +74,3 @@ public class SecurityConfig {
         //@formatter:on
     }
 }
-
-
--- 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
+++ 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/FileController.java
@@ -10,7 +10,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
